9aa1db5251e95b244d16374eecffd1077363b4bc
ObserverOfTime <chronobserver@disroot.org>
2025-07-19 12:15:21 +0300

    feat!: update bindings

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,13 +3,13 @@ package tree_sitter_pymanifest_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter-grammars/tree-sitter-pymanifest"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_pymanifest "github.com/tree-sitter-grammars/tree-sitter-pymanifest/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
 	language := tree_sitter.NewLanguage(tree_sitter_pymanifest.Language())
 	if language == nil {
-		t.Errorf("Error loading Pymanifest grammar")
+		t.Errorf("Error loading PyPA manifest grammar")
 	}
 }
